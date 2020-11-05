2996955296db714728b2d6abe224787733509ef8
Kamran Afzali <kamranafzali@Kamrans-MBP.lan>
2020-11-05 14:30:43 -0500

    commit5

--- Technical-analysis/code.R
+++ Technical-analysis/code.R
@@ -1,6 +1,8 @@
 rm(list=ls())
 install.packages("quantmod")
 install.packages("ggplot2")
+install.packages("PerformanceAnalytics")
+library(PerformanceAnalytics)
 library(quantmod)
 library(ggplot2)
 library(tidyverse)
@@ -32,3 +34,29 @@ chartSeries(spy,
             subset='2013::2016',
             theme=chartTheme('white'))
 addMACD(fast=12,slow=26,signal=9,type="EMA")
+
+
+
+day <-14
+price <- Cl(spy)
+signal <- c()                    #initialize vector
+rsi <- RSI(price, day)     #rsi is the lag of RSI
+signal [1:day+1] <- 0            #0 because no signal until day+1
+
+for (i in (day+1): length(price)){
+  if (rsi[i] < 30){             #buy if rsi < 30
+    signal[i] <- 1
+  }else {                       #no trade all if rsi > 30
+    signal[i] <- 0
+  }
+}
+signal<-reclass(signal,Cl(spy))
+trade1 <- Lag(signal)
+
+#construct a new variable ret1
+ret1 <- dailyReturn(spy)*trade1
+names(ret1) <- 'Naive'
+charts.PerformanceSummary(ret1)
+
+
+charts.PerformanceSummary(ret1, main="Naive v.s. RSI")
