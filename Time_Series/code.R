253dd607dc225b20cdc1d3dbe52f6ee6c2959be3
Kamran Afzali <36087887+Kamran-Afzali@users.noreply.github.com>
2020-11-26 10:03:54 -0500

    Update code.R

--- Time_Series/code.R
+++ Time_Series/code.R
@@ -11,3 +11,13 @@ library(forecast)
 
 
 BTC <- getSymbols("BTC-USD", src = "yahoo", from = "2013-01-01", to = "2020-11-01", auto.assign = FALSE)
+
+Op(BTC)
+Hi(BTC)
+Lo(BTC)
+Cl(BTC)
+Vo(BTC)
+Ad(BTC)
+
+#dailyReturn(spy)
+plot(dailyReturn(BTC))
