e3642ddf8ae4b977fbe27a7740585457f9fd3e00
Kamran Afzali <k.afzali@gmail.com>
2021-07-19 12:02:13 -0400

    notif1

--- Technical-analysis/code.R
+++ Technical-analysis/code.R
@@ -51,156 +51,89 @@ plot(quarterlyReturn(spy))
 yearlyReturn(spy)
 plot(yearlyReturn(spy))
 
-SMA(Ad(spy),n=20)
-EMA(Ad(spy),n=20)
+SMA(Cl(spy),n=20)
+EMA(Cl(spy),n=20)
 chartSeries(spy,
             subset='2013::2016',
             theme=chartTheme('white'))
 addMACD(fast=12,slow=26,signal=9,type="EMA")
 
 
-signal1=1*(RSI(Ad(spy))< 30)
+signal1=1*(RSI(Cl(spy))< 30)
 trade1 <- Lag(signal1)
 trade1[is.na(trade1)]=0
 ret1 <- dailyReturn(spy)*trade1
 sum(ret1)
 charts.PerformanceSummary(ret1)
 
-RSI(Ad(spy),n=5)
+RSI(Cl(spy),n=5)
 
-signal2=1*(EMA(Ad(spy),n=12)>EMA(Ad(spy),n=26))
+signal2=1*(EMA(Cl(spy),n=12)>EMA(Cl(spy),n=26))
 trade2 <- Lag(signal2)
 trade2[is.na(trade2)]=0
 ret2 <- dailyReturn(spy)*trade2
 sum(ret2)
 charts.PerformanceSummary(ret2)
 
-signal2=1*(EMA(Ad(spy),n=24)>EMA(Ad(spy),n=52))
+signal2=1*(EMA(Cl(spy),n=24)>EMA(Cl(spy),n=52))
 trade2 <- Lag(signal2)
 trade2[is.na(trade2)]=0
 ret2 <- dailyReturn(spy)*trade2
 sum(ret2)
 charts.PerformanceSummary(ret2)
 
-signal3=1*(MACD(Ad(spy), nSig = 18)$signal>0)
+signal3=1*(MACD(Cl(spy), nSig = 18)$signal>0)
 trade3 <- Lag(signal3)
 trade3[is.na(trade3)]=0
 ret3 <- dailyReturn(spy)*trade3
 sum(ret3)
 charts.PerformanceSummary(ret3)
 
-signal_RSI=1*(RSI(Cl(spy))< 30)%>%Lag()%>% replace(is.na(.), 0)
-signal_EMA1=1*(EMA(Cl(spy),n=12)>EMA(Cl(spy),n=26))%>%Lag()%>% replace(is.na(.), 0)
-signal_EMA2=1*(EMA(Cl(spy),n=24)>EMA(Cl(spy),n=52))%>%Lag()%>% replace(is.na(.), 0)
-signal_MACD1=1*(MACD(Cl(spy))$signal>0)%>%Lag()%>% replace(is.na(.), 0)
-signal_MACD2=1*(MACD(Ad(spy), nSig = 18)$signal>0)%>%Lag()%>% replace(is.na(.), 0)
 
 
 
-ETHQ.TO
-CTS.V
-RSI.TO
-GDNP.V
-LSPD.TO
-ATE.TO
-EDGE.TO
-ZEO.TO
-ZRE.TO
-RCI-B.TO
-BTO.TO
-ABX.TO
-NWH-UN.TO
-SJR-B.TO
-IHI
-XEG.TO
-XMA.TO
-XIN.TO
-XMD.TO
-XIT.TO
-SU.TO
-MFC.TO
-TRP.TO
-T.TO
-BEPC.TO
-KL.TO
-CGL.TO
-APHA.TO
-SKYY
-RNW.TO
-PSI
-ICLN
-HEO.V
-ABT.TO
-ARKK
-ETHQ.TO
-CTS.V
-RSI.TO
-GDNP.V
-LSPD.TO
-ATE.TO
-IPA.V
-LIFE.TO
-MCHI
-XCH.TO
-IHF
-ZEO.TO
-XGD.TO
-ZRE.TO
-HACK
-RCI-B.TO
-BTO.TO
-ABX.TO
-NWH-UN.TO
-XTR.TO
-XEG.TO
-XMA.TO
-XIN.TO
-XMD.TO
-XIT.TO
-BIP-UN.TO
-AP-UN.TO
-RTH
-TAN
-PBD
-EMQQ
-KGRN
-VDY.TO
-ROBO
-BLDP.TO
-AC.TO
-DGRO
-BCE.TO
-ATZ.TO
-ARRY
-ZCH.TO
-REI-UN.TO
-BB.TO
-ABT.TO
-GNOM
-ARKG
-VGRO.TO
-SCHD
-WCN.TO
-RBA.TO
-VRE.TO
-XDSR.TO
-BEP-UN.TO
-XEQT.TO
-XQQ.TO
-VFV.TO
-CNR.TO
-XUH.TO
-XUU.TO
-PSI
-IGV
-PSJ
-SOXX
-IHI
-IVW
-XST.TO
-XUT.TO
-CPX.TO
-HXS.TO
-HXQ.TO
-XHC.TO
+cbind(
+signal_RSI=1*(RSI(Cl(spy))< 30)%>%Lag()%>% replace(is.na(.), 0),
+signal_EMA1=1*(EMA(Cl(spy),n=12)>EMA(Cl(spy),n=26))%>%Lag()%>% replace(is.na(.), 0),
+signal_EMA2=1*(EMA(Cl(spy),n=24)>EMA(Cl(spy),n=52))%>%Lag()%>% replace(is.na(.), 0),
+signal_MACD1=1*(MACD(Cl(spy))$signal>0)%>%Lag()%>% replace(is.na(.), 0),
+signal_MACD2=1*(MACD(Cl(spy), nSig = 18)$signal>0)%>%Lag()%>% replace(is.na(.), 0),
+dailyreturn=dailyReturn(spy)%>%Lag()%>% replace(is.na(.), 0),
+weeklyreturn=SMA(dailyReturn(spy),n=5)%>%Lag()%>% replace(is.na(.), 0))
 
+tkrs=unique(c("ETHQ.TO","CTS.V","RSI.TO","GDNP.V","LSPD.TO","ATE.TO","EDGE.TO","ZEO.TO","ZRE.TO","RCI-B.TO","BTO.TO","ABX.TO","NWH-UN.TO","SJR-B.TO","IHI","XEG.TO","XMA.TO","XIN.TO","XMD.TO","XIT.TO","SU.TO","MFC.TO","TRP.TO","T.TO","BEPC.TO","KL.TO","CGL.TO","APHA.TO","SKYY","RNW.TO","PSI","ICLN","HEO.V","ABT.TO","ARKK","ETHQ.TO","CTS.V","RSI.TO","GDNP.V","LSPD.TO","ATE.TO","IPA.V","LIFE.TO","MCHI","XCH.TO","IHF","ZEO.TO","XGD.TO","ZRE.TO","HACK","RCI-B.TO","BTO.TO","ABX.TO","NWH-UN.TO","XTR.TO","XEG.TO","XMA.TO","XIN.TO","XMD.TO","XIT.TO","BIP-UN.TO","AP-UN.TO","RTH","TAN","PBD","EMQQ","KGRN","VDY.TO","ROBO","BLDP.TO","AC.TO","DGRO","BCE.TO","ATZ.TO","ARRY","ZCH.TO","REI-UN.TO","BB.TO","ABT.TO","GNOM","ARKG","VGRO.TO","SCHD","WCN.TO","RBA.TO","VRE.TO","XDSR.TO","BEP-UN.TO","XEQT.TO","XQQ.TO","VFV.TO","CNR.TO","XUH.TO","XUU.TO","PSI","IGV","PSJ","SOXX","IHI","IVW","XST.TO","XUT.TO","CPX.TO","HXS.TO","HXQ.TO","XHC.TO"))
+
+
+
+spy <- getSymbols("SPY", src = "yahoo", from = "2012-07-19", to = "2019-07-19", auto.assign = FALSE)
+buy_trd_crt=(1*(RSI(Cl(spy))< 30)%>%Lag()%>% replace(is.na(.), 0)+
+    1*(EMA(Cl(spy),n=12)>EMA(Cl(spy),n=26))%>%Lag()%>% replace(is.na(.), 0)+
+    1*(EMA(Cl(spy),n=24)>EMA(Cl(spy),n=52))%>%Lag()%>% replace(is.na(.), 0)+
+    1*(MACD(Cl(spy))$signal>0)%>%Lag()%>% replace(is.na(.), 0)+
+    1*(MACD(Cl(spy), nSig = 18)$signal>0)%>%Lag()%>% replace(is.na(.), 0))
+
+sell_trd_crt=(1*(RSI(Cl(spy))> 70)%>%Lag()%>% replace(is.na(.), 0)+
+               1*(EMA(Cl(spy),n=12)<EMA(Cl(spy),n=26))%>%Lag()%>% replace(is.na(.), 0)+
+               1*(EMA(Cl(spy),n=24)<EMA(Cl(spy),n=52))%>%Lag()%>% replace(is.na(.), 0)+
+               1*(MACD(Cl(spy))$signal<0)%>%Lag()%>% replace(is.na(.), 0)+
+               1*(MACD(Cl(spy), nSig = 18)$signal<0)%>%Lag()%>% replace(is.na(.), 0))
+
+
+
+sum(dailyReturn(spy))
+length(dailyReturn(spy))
+
+ret <- dailyReturn(spy)*(1*(buy_trd_crt>0))+ dailyReturn(spy)*(1*!(sell_trd_crt>0))
+sum(ret)
+
+ret <- dailyReturn(spy)*(1*(buy_trd_crt>0))
+sum(ret)
+
+ret <-  dailyReturn(spy)*(1*!(sell_trd_crt>0))
+sum(ret)
+
+
+sum((trd_crt>0))
+
+
+sum(!(dailyReturn(spy)*(1*(buy_trd_crt>0))+ dailyReturn(spy)*(1*!(sell_trd_crt>0))==0))
